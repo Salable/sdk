055e27b71b3eaae2a9f8b82fc89c21b8541cd1b2
Coner Murphy <conermurphy@users.noreply.github.com>
2026-09-11 12:09:27 +0100

    fix: enum write error and bump kiota

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import {ApiKeyAuthenticationProvider, ApiKeyLocation} from "@microsoft/kiota-abstractions";
+import {ApiKeyAuthenticationProvider, ApiKeyLocation, type SerializationWriterFactoryRegistry} from "@microsoft/kiota-abstractions";
 import {FetchRequestAdapter} from "@microsoft/kiota-http-fetchlibrary";
 import {createSalable} from "./sdk/salable";
+import {NullSafeJsonSerializationWriterFactory} from "./serialization";
 
 export class Salable {
   public readonly api: ReturnType<typeof createSalable>['api'];
@@ -13,5 +14,11 @@ export class Salable {
     );
     const adapter = new FetchRequestAdapter(authProvider);
     this.api = createSalable(adapter).api;
+    // Must run after createSalable, which registers the stock JSON serializer;
+    // re-registering replaces the application/json factory with the null-safe one.
+    const writerRegistry = adapter.getSerializationWriterFactory() as SerializationWriterFactoryRegistry;
+    if (writerRegistry.registerDefaultSerializer) {
+      writerRegistry.registerDefaultSerializer(NullSafeJsonSerializationWriterFactory);
+    }
   }
 }
